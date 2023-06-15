e484e86c47ab307f72d2de7a4e7857c4310f6834
iamanggi <anggipuspita452@gmail.com>
2023-06-15 15:09:34 +0700

    membuat void display

--- StackArray/StackArray.cpp
+++ StackArray/StackArray.cpp
@@ -38,4 +38,15 @@ public:
 	bool empty() {
 		return (top == -1);
 	}
+
+	void display() {
+		if (empty()) {
+			cout << "\nStack is empty." << endl;
+		}
+		else {
+			for (int tmp = 0; tmp <= top; tmp++) {
+				cout << stack_array[tmp] << endl;
+			}
+		}
+	}
 };
